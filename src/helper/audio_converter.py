841714d99b2f7a7f24312c528b5834510ca4c4e7
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2025-05-18 12:34:29 -0300

    fix: replace python-ffmpeg with ffmpeg-python in dependencies 🐍

--- src/helper/audio_converter.py
+++ src/helper/audio_converter.py
@@ -1,4 +1,4 @@
-from ffmpeg import FFmpeg
+import ffmpeg
 from helper.chronometer import Chronometer
 
 
@@ -8,9 +8,9 @@ class AudioConverter:
         chronometer.start()
 
         print("Converting m4a to wav...")
-        FFmpeg().input(input_file_path).output(
-            output_file_path, ar=16000, ac=1
-        ).execute()
+        ffmpeg.input(input_file_path).output(output_file_path, ar=16000, ac=1).run(
+            quiet=True
+        )
 
         chronometer.stop()
         elapsed_time = chronometer.elapsed_time(True)
