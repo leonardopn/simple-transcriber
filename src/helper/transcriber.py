fed667801cf40b4efdc7514fa5a6284411b8f7e1
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2025-05-18 10:40:13 -0300

    feat: add Transcriber class for audio transcription using Whisper 🎤

---
+++ src/helper/transcriber.py
@@ -0,0 +1,41 @@
+import os
+from typing import Literal
+import whisper
+from helper.chronometer import Chronometer
+
+Model = Literal["tiny", "base", "small", "medium", "large", "turbo"]
+Language = Literal["en", "pt"]
+
+
+class Transcriber:
+
+    def transcribe_audio(
+        self, audio_path: str, model_name: Model, language: Language
+    ) -> str:
+        """Transcreve o áudio usando Whisper."""
+
+        chronometer = Chronometer()
+        chronometer.start()
+
+        print("")
+        print("Loading whisper model...")
+        model = whisper.load_model(model_name)
+        print("Model loaded.")
+
+        print("Transcribing audio...")
+        result = model.transcribe(audio_path, language=language)
+
+        parent_path = os.path.dirname(audio_path)
+        output_file = os.path.join(parent_path, "transcription.txt")
+
+        with open(output_file, "w", encoding="utf-8") as f:
+            f.write(result["text"])
+
+        chronometer.stop()
+        elapsed_time = chronometer.elapsed_time(True)
+        print(
+            f"[{elapsed_time}] - Transcription complete. Output saved to:", output_file
+        )
+        print("")
+
+        return result["text"]
